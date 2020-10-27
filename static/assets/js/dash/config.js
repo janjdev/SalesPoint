984b76d20af69b994a4eade1a84af50a9e8385b6
jjansa <jpjansa@gmail.com>
2020-10-27 11:17:09 -0500

    preparing for deployment

---
+++ static/assets/js/dash/config.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+   
+    const fontList = document.querySelector('#fontlist');
+
+    fontList.addEventListener('change', function(){
+
+    })
+
+});
